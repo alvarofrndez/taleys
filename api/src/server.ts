dc2cb1e578789c575bd220b4c0e17900c2d8c42d
alvaro <alvarofdayala@gmail.com>
2025-10-08 14:31:33 +0200

    Solucion de errores para deploy cond module alias2

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,6 @@
+import 'module-alias/register'
 import app from "./index"
 import { env } from './config/config_env'
-import 'module-alias/register'
 
 app.listen(env.PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${env.PORT}`)
