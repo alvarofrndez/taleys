3071b9581a084763879eb523b27ec583ae969f9e
alvaro <alvarofdayala@gmail.com>
2025-10-08 14:19:18 +0200

    Solucion de errores para deploy

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import app from "./index"
 import { env } from './config/config_env'
+import 'module-alias/register'
 
 app.listen(env.PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${env.PORT}`)
