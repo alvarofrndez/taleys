b9dea815123e3178ae78662abf7ed66e9e36ea21
alvaro <alvarofdayala@gmail.com>
2025-10-10 18:02:23 +0200

    Solucion de errores para deploy cookies

--- api/src/utils/cookies/setAuthToken.ts
+++ api/src/utils/cookies/setAuthToken.ts
@@ -2,10 +2,12 @@ import { Response } from 'express'
 import { env } from '@/config/config_env'
 
 const setAuthToken = (res: Response, token: string, refresh_token: string = null) => {
+    const is_local = env.NODE_ENV === 'local'
+    
     res.cookie('token', token, {
         httpOnly: true,
-        secure: env.NODE_ENV === 'production',
-        sameSite: env.NODE_ENV === 'production' ? 'strict' : 'lax',
+        secure: !is_local,
+        sameSite: !is_local ? 'none' : 'lax',
         maxAge: env.JWT_EXPIRES_IN,
         path: '/'
     })
@@ -13,8 +15,8 @@ const setAuthToken = (res: Response, token: string, refresh_token: string = null
     if(refresh_token){
         res.cookie('refresh_token', refresh_token, {
             httpOnly: true,
-            secure: env.NODE_ENV === 'production',
-            sameSite: env.NODE_ENV === 'production' ? 'strict' : 'lax',
+            secure: !is_local,
+            sameSite: !is_local ? 'none' : 'lax',
             maxAge: env.JWT_EXPIRES_IN_REFRESH,
             path: '/'
         })
--- api/src/utils/redis/index.ts
+++ api/src/utils/redis/index.ts
@@ -1,7 +1,18 @@
 import Redis from 'ioredis'
 import { env } from '@/config/config_env'
 
-const redis = new Redis(env.REDIS_URL)
+let redis
+
+if(env.NODE_ENV === 'local'){
+  redis = new Redis({
+    host: '127.0.0.1',
+    port: 6379,
+  })
+}else{
+  redis = new Redis(env.REDIS_URL!, {
+    tls: {}
+  })
+}
 
 redis.on('connect', () => console.log('🟢 Conectado a Redis'))
 redis.on('error', (err) => console.error('🔴 Error en Redis:', err))
