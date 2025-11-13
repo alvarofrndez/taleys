7908857d1a124644e0a5984a5c6f0359d4e79405
alvaro <alvarofdayala@gmail.com>
2025-11-13 15:27:44 +0100

    Quitar react-spinner

--- web/components/Loader.js
+++ web/components/Loader.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import { ClipLoader } from 'react-spinners';
-import styles from '@/assets/global/loader.module.scss';
+import styles from '@/assets/global/loader.module.scss'
+import React from 'react'
 
 const Loader = ({ color = 'background', size = 30 }) => {
-  const spinnerColor =
-    color === 'foreground'
-      ? 'var(--color-primary-foreground)'
-      : 'var(--color-primary)';
+    return (
+        <div className={`${styles.loader} ${color === 'foreground' ? styles.foreground : ''}`} style={{ width: size, height: size }}>
+            <div className={styles.loader_spinner}></div>
+        </div>
+    )
+}
 
-  return (
-    <div
-      className={`${styles.loader} ${color === 'foreground' ? styles.foreground : ''}`}
-      style={{ width: size, height: size, display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-    >
-      <ClipLoader size={size} color={spinnerColor} />
-    </div>
-  );
-};
-
-export default Loader;
+export default Loader
